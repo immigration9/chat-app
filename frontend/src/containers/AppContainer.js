576e7f23db1edacf60f89f53f9ac1630811a4004
MinGu Lee <mglee@whatap.io>
2019-07-12 06:24:20 +0900

    readme added

--- frontend/src/containers/AppContainer.js
+++ frontend/src/containers/AppContainer.js
@@ -1,14 +1,12 @@
 import React, { Component, Fragment } from 'react';
 import { Switch } from 'react-router-dom';
 import CoreLayout from 'layouts/CoreLayout';
-import { connect } from 'react-redux';
 
 import Login from 'containers/Login/Login';
 import ChatroomList from 'containers/Chatroom/ChatroomList';
 import Chatroom from 'containers/Chatroom/Chatroom';
 import PageNotFound from 'containers/Chatroom/Chatroom';
 import AddChatroom from './Chatroom/AddChatroom';
-import ParticipantList from './Chatroom/ParticipantList';
 import SocketHandler from 'utils/socket';
 
 class AppContainer extends Component {
@@ -23,7 +21,6 @@ class AppContainer extends Component {
           <CoreLayout exact path={"/chatrooms"} component={ChatroomList} />
           <CoreLayout exact path={"/chatrooms/add"} component={AddChatroom} />
           <CoreLayout exact path={"/chatroom/:chatroomId"} component={Chatroom} />
-          <CoreLayout exact path={"/chatroom/:chatroomId/list"} component={ParticipantList} />
 
           <CoreLayout component={PageNotFound} />
         </Switch>
